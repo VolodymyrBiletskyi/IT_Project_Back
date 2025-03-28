f28206c968427558dcd8ca1c28cd59ee63842f2c
Billy <opass@edu.cdv.pl>
2025-03-28 20:34:14 +0100

    added assets

---
+++ paw-and-care/src/pages/Error.js
@@ -0,0 +1,5 @@
+﻿const Error = () => {
+  return <h1>Contact Me</h1>;
+};
+
+export default Error;
---
+++ paw-and-care/src/pages/Services.js
@@ -0,0 +1,5 @@
+﻿const Services = () => {
+  return <h1>Contact Me</h1>;
+};
+
+export default Services;
