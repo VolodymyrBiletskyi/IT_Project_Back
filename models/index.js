fc1eabba5302400deb522ca5655f2f8aa7fc5340
Outlines1 <danufriiev@edu.cdv.pl>
2025-04-13 15:23:15 +0200

    new relations in db

---
+++ models/index.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const process = require('process');
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const User = require('./User');
+const Pet = require('./Pet');
+const Appointment = require('./Appointment');
+const config = require(__dirname + '/../config/config.json')[env];
+const db = {};
+
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
+User.hasMany(Pet, { foreignKey: 'user_id' });
+Pet.belongsTo(User, { foreignKey: 'user_id' });
+
+User.hasMany(Appointment, { foreignKey: 'user_id' });
+Appointment.belongsTo(User, { foreignKey: 'user_id' });
+
+Appointment.belongsTo(Pet, { foreignKey: 'pet_id' });
+Pet.hasMany(Appointment, { foreignKey: 'pet_id' });
+
+module.exports = { User, Pet, Appointment };
+
+fs
+  .readdirSync(__dirname)
+  .filter(file => {
+    return (
+      file.indexOf('.') !== 0 &&
+      file !== basename &&
+      file.slice(-3) === '.js' &&
+      file.indexOf('.test.js') === -1
+    );
+  })
+  .forEach(file => {
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
