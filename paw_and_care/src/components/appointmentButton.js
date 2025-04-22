c4e7adc195c75bcccb5d9b66c45911e8ff2a2138
Billy <opass@edu.cdv.pl>
2025-04-23 01:18:22 +0200

    appointment button added

---
+++ paw_and_care/src/components/appointmentButton.js
@@ -0,0 +1,14 @@
+﻿import React from 'react';
+import { Link } from 'react-router-dom';
+import './appointmentButton.css';
+
+const AppointmentButton = () => {
+  return (
+  <div className="appointment-btn">
+
+    <Link to="appointment-request"><button className="orange-btn">Request Appointment</button></Link>
+  </div>
+  )
+}
+
+export default AppointmentButton;
