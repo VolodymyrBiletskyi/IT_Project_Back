82b16bdbd4d55b66e99ad7d03ec67ebf24268dd4
Billy <opass@edu.cdv.pl>
2025-04-26 18:38:47 +0200

    view services button added

---
+++ paw_and_care/src/components/allServicesButton.js
@@ -0,0 +1,14 @@
+﻿import { Link } from "react-router-dom";
+import React from "react";
+import arrow from "../assets/SVG/arrow-right.svg";
+import './allServicesButton.css';
+
+const ViewAllServices = () => {
+  return (
+    <div className="viewAll-btn">
+      <Link to="services"><button className="white-btn">View All Services <img src={arrow} alt={arrow}/></button></Link>
+    </div>
+  )
+}
+
+export default ViewAllServices;
