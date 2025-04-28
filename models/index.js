6f66dd8b8501e0fe81b565817028f255fc4c5fe9
Outlines1 <danufriiev@edu.cdv.pl>
2025-04-28 10:08:06 +0200

    changed index.js to be static (maybe will be changed)

--- models/index.js
+++ models/index.js
@@ -6,12 +6,10 @@ const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const User = require('./User');
-const Pet = require('./Pet');
-const Appointment = require('./Appointment');
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require(__dirname + '/../config/database-migration')[env];
 const db = {};
 
+// Initialize Sequelize instance
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
@@ -19,39 +17,9 @@ if (config.use_env_variable) {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
-User.hasMany(Pet, { foreignKey: 'owner_id', onDelete: 'SET NULL' }); // Set user_id to NULL if the user is deleted
-Pet.belongsTo(User, { foreignKey: 'owner_id', constraints: false });
+const Specialist = require('./Specialist')(sequelize, Sequelize.DataTypes);
+db['Specialist'] = Specialist;
 
-User.hasMany(Appointment, { foreignKey: 'user_id' });
-Appointment.belongsTo(User, { foreignKey: 'user_id' });
-
-Appointment.belongsTo(Pet, { foreignKey: 'pet_id' });
-Pet.hasMany(Appointment, { foreignKey: 'pet_id' });
-
-module.exports = { User, Pet, Appointment };
-
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (
-      file.indexOf('.') !== 0 &&
-      file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
-    );
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
-
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+module.exports = db;
 
 module.exports = db;
