2beabe1587750b19bb023cc968de3a391cd6b279
VanDarkHolme <vbiletskyi@edu.cdv.pl>
2025-05-15 19:21:27 +0200

    added host dabase connection

--- config/database.js
+++ config/database.js
@@ -1,15 +1,28 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
+const connectionString = process.env.DATABASE_URL;
+
+const sequelize = connectionString
+  ? new Sequelize(connectionString, {
     dialect: process.env.DB_DIALECT || "postgres",
     logging: false,
-  }
-);
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  })
+  : new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+      host: process.env.DB_HOST,
+      dialect: process.env.DB_DIALECT || "postgres",
+      logging: false,
+    }
+  );
 
 module.exports = sequelize;
