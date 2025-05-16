c13900726affcf5175e64febea5a1e50f8667926
Billy <opass@edu.cdv.pl>
2025-05-16 23:24:22 +0200

    "More" button added

---
+++ paw_and_care/src/components/moreButton.js
@@ -0,0 +1,11 @@
+﻿import React from 'react';
+import { Link } from 'react-router-dom';
+import './moreButton.css';
+
+const MoreButton = () => {
+  return (
+      <Link to="/about-us"><button className="blue-btn">More</button></Link>
+  )
+}
+
+export default MoreButton;
