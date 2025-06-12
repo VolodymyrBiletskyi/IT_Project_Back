b7f6e67b233da3f5170dd43727111401af3455c0
Billy <opass@edu.cdv.pl>
2025-06-12 15:48:54 +0200

    files restored

---
+++ paw_and_care/src/components/sideMenu/SideMenu.js
@@ -0,0 +1,29 @@
+﻿import React from 'react';
+import { Link } from 'react-router-dom';
+import '../../pages/Profile/Profile.css';
+import { useLocation } from 'react-router-dom';
+
+
+const SideMenu = () => {
+  const location = useLocation();
+  return (
+    <div className="main-section">
+      <div className="menu-container">
+        <ul>
+          <Link
+            to={'/profile/pets'} className={'pet-link'} style={location.pathname === '/profile/pets' ? { background: '#086788', color: 'white' } : {}}>
+            <ol>Pet</ol>
+          </Link>
+          <Link to={'/profile/medical-records'} className={'med-records'} style={location.pathname === '/profile/medical-records' ? { background: '#086788', color: 'white' } : {}}>
+            <ol>Medical Records</ol>
+          </Link>
+          <Link to={'/profile/visits'} className={'visits'} style={location.pathname === '/profile/visits' ? { background: '#086788', color: 'white' } : {}}>
+            <ol>Visits</ol>
+          </Link>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default SideMenu;
---
+++ paw_and_care/src/pages/Profile/profilePages/PetPage.js
@@ -0,0 +1,31 @@
+﻿import React from "react";
+import Header from "../../../components/header/header";
+import Footer from "../../../components/footer/footer";
+import SideMenu from "../../../components/SideMenu/SideMenu";
+import '../Profile.css'
+import dog from '../../../assets/Pet profile/Labrador.jpg';
+
+
+const PetPage = () => {
+  return (
+    <div className={'pet-page'}>
+      <Header/>
+      <div className={'main-section'}>
+        <h1 >Profile</h1>
+        <div className={'grid-container'}>
+          <SideMenu/>
+          <div className={'pet-card'}>
+            <img src={dog} alt={dog} />
+            <p>Name</p>
+            <p>Species:</p>
+            <p>Breed:</p>
+            <p>Gender:</p>
+          </div>
+        </div>
+      </div>
+      <Footer/>
+    </div>
+  )
+}
+
+export default PetPage;
